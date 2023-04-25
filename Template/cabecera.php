9c14b8698d4af8e01b4a26b83f268c5645149f2e
faryalex <fary_alex@outlook.com>
2023-04-24 20:48:48 -0500

    actualizacion 24/04/2023

--- Template/cabecera.php
+++ Template/cabecera.php
@@ -14,7 +14,7 @@
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-<body>
+
 <nav class="navbar navbar-expand-lg b">
   <div class="container">
     <a class="navbar-brand" href="#">
@@ -79,3 +79,4 @@
   </div>
 </nav>
 
+<body>
--- Ventanas/sobre_nosotros.php
+++ Ventanas/sobre_nosotros.php
@@ -1,72 +1,58 @@
 <?php include("../Template/cabecera.php"); ?>
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../Style/sobrenosotros.css" />
 </head>
 <body>
-<div class="cards">
-      <div class="card">
-        <div class="header">
-          <div class="date">¿QUIENES SOMOS?</div>
-          <div class="title">QUIENES SOMOS</div>
-        </div>
-        <div class="footer">
-          <div class="author">
-            <a href="#">
-              <img src="" alt="" />
-              <span>CharyCris</span>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="card element-1">
-        <div class="header">
-          <div class="date">MISION</div>
-          <div class="title">Nuestra mision es fomentar en nuestros clientes el cosnumo de productos que no dañen su salud y mejoren su calidad de vida</div>
-        </div>
-        <div class="footer">
-          <div class="author">
-            <a href="#">
-              <img src="" alt="" />
-              <span>CharyCris</span>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="card element-2">
-        <div class="header">
-          <div class="date">CharyCris</div>
-          <div class="title">Vision </div>
-        </div>
-        <div class="footer">
-          <div class="author">
-            <a href="#">
-              <img src="" alt="" />
-              <span>CharyCris</span>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="card element-3">
-        <div class="header">
-          <div class="date">NUESTROS DESARROLLADORES </div>
-          <div class="title">
-           <a href="../Ventanas/desarrolladores.php">Visita informacion de los desarrolladores </a>
- 
-          </div> 
-        </div>
-        <div class="footer">
-          <div class="author">
-            <a href="#">
-              <img src="" alt="" />
-              <span>CharyCris</span>
-            </a>
-          </div>
-        </div>
-      </div>
+    <section  id="services-sec">
+        <div class="container">
+            <div class="row ">
+                <h1 class="g-pad-bottom">  <i class="fa fa-crosshairs "></i> Sobre Nosotros  </h1>
+                
+               
+                <div class="text-center g-pad-bottom">
+                    <div class="col-md-4 col-sm-4">
+                            <i class="fa fa-fire-extinguisher fa-5x faa-vertical animated c-main "></i>
+                            <h4 >Mision de CharyCris </h4>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                            
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                            <i class="fa fa-coffee fa-5x faa-ring animated c-main "></i>
+                            <h4>Vision de CharyCris </h4>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Curabitur nec nisl odio. Mauris vehicula at nunc id posuere.
+                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </p>
+                            
+                    </div>
+                   
+                    <div class="col-md-4 col-sm-4 d">
+    <i class="fa fa-gavel fa-5x faa-shake animated c-main "></i>
+    <h4>Contactanos y visita la informacion de nuestros desarrolladores </h4>
+    <div class="btn-group btn-group-justified">
+        <a href="../Ventanas/contactoo.html" class="btn mr-2">Visitar Redes</a>
+        <a href="../Ventanas/desarrolladores.html" class="btn mr-2">Ver Desarrolladores</a>
     </div>
+</div>
+
+                </div>
+                  </div>
+            
+          
+        </div>
+    </section>
 </body>
+</html>
